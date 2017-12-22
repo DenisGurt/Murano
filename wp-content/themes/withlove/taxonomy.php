95d05d202ec2d84749c4b320d3004a680ce622d1
Darth Vader <denis.gurtovenko23@gmail.com>
2017-12-22 22:15:23 +0200

    taxonomy sidebar

--- wp-content/themes/withlove/taxonomy.php
+++ wp-content/themes/withlove/taxonomy.php
@@ -11,7 +11,14 @@
  * @since 1.0
  * @version 1.0
  */
-$current_term_slug = get_query_var('product_cat');
+$term_id = get_queried_object()->term_id;
+$term_slug = get_query_var('product_cat');
+$image = get_field('cat_bg', 'term_'.$term_id);
+$bg_src = get_stylesheet_directory_uri() . '/assets/images/slide1.jpg';
+
+if( $image ) {
+    $bg_src = $image['sizes']['wl-large'];
+}
 
 get_header(); ?>
 
@@ -19,6 +26,6 @@ get_header(); ?>
 
 <?php get_sidebar(); ?>
 
-<section class="section__bg section__<?php echo $current_term_slug; ?>"></section>
+<section class="section section__bg" style="background-image: url(<?php echo $bg_src; ?>);"></section>
 
 <?php get_footer(); ?>
